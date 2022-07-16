d7219687235801a7f3554226adbd7e9262df2d2f
Nadil Mohammad <nimohammad18@cit.just.edu.jo>
2022-07-16 20:48:04 +0300

    Time: 1 ms (98.83%), Space: 50.5 MB (15.13%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.java
+++ 1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -1,18 +1,3 @@
-// class Solution {
-//     public int[] getConcatenation(int[] nums) {
-//         int size = nums.length;
-      
-//         int [] ans  = new int [size*2];
-//         for(int i=0; i<size;i++){
-//             ans[i]=nums[i];
-//         }
-//         for(int i=0; i<size;i++){
-//             ans[i+size]=nums[i];
-           
-//         }
-//         return ans;
-//     }
-// }
 
 class Solution {
     public int[] getConcatenation(int[] nums) {
